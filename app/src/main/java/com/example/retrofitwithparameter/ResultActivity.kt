f5388043fe035cdd1c6a2458cc00be95887f7d81
Mangeshd09 <mangeshdhasade123@gmail.com>
2022-06-03 18:05:38 +0530

    Retrofit With parameter enter username and password , display data with image to next activity.

--- app/src/main/java/com/example/retrofitwithparameter/ResultActivity.kt
+++ app/src/main/java/com/example/retrofitwithparameter/ResultActivity.kt
@@ -1,10 +1,14 @@
 package com.example.retrofitwithparameter
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.retrofitwithparameter.databinding.ActivityResultBinding
 import okhttp3.OkHttpClient
 import retrofit2.Call
@@ -23,9 +27,15 @@ class ResultActivity : AppCompatActivity() {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_result)
 
+        //"https://kintree.com/preprod/${result.result.userImage}"
+        Glide.with(applicationContext)
+            .load("https://kintree.com/preprod/${result.result.userImage}")
+            .error(R.drawable.ic_baseline_image_24)
+            .into(binding.imageView)
+
         binding.userFirstNameText.text = "First Name: " + result.result.userFirstName +
                 "Auth Token :" + result.result.authToken + "User Id: " + result.result.userId +
-                "User Language :" + result.result.userLang
+                "User Language :" + result.result.userLang + "IMAGE URL :" + result.result.userImage
 
     }
 }
